ae88f075020758e954f9819da83278f5d0de56d2
Adebozz <ademolaadeigbe3@gmail.com>
2023-10-21 16:00:37 +0100

    update

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./HeroStyles.css";
+
+function Hero() {
+  return (<> 
+    <div className='hero'>
+      <img alt='HeroImg' src=''/>
+    </div>
+    </>
+   
+  )
+}
+
+export default Hero
