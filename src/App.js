17d4d6ca681585db37695936891e1fbb37ca0267
Adebozz <ademolaadeigbe3@gmail.com>
2023-10-21 03:01:56 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './styles.css';
 import Navbar from './components/Navbar'
+import Home from './routes/Home';
+import About from './routes/About';
+import { Route, Routes } from 'react-router-dom';
+import Contact from './routes/Contacts';
+import Service from './routes/Services';
 // import { Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/service" element={<Service/>} />
+        </Routes>
+
         <Navbar />
-      
+       
       
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import "./Navbar.css";
 import { MenuItems } from "./MenuItems";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Navbar extends Component{
     state = {clicked: false};
@@ -20,10 +20,10 @@ class Navbar extends Component{
                     {MenuItems.map((item, index) =>{
                         return(
                             <li key={index}>
-                        <a className={item.cName} href={item.url}>
+                        <Link className={item.cName} to={item.url}>
                         <i className={item.icon}></i>
                         {item.tittle}
-                         </a>
+                         </Link>
                     </li> 
                         )
                     })
