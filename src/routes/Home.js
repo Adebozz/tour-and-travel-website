9740fd9a36376b2c80b28692335aa386a1c2629d
Adebozz <ademolaadeigbe3@gmail.com>
2023-10-30 17:54:48 +0100

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
+
 
 function Home (){
     return( 
@@ -8,7 +9,7 @@ function Home (){
         <Navbar />
         <Hero 
         cName="hero"
-        heroImg="/"
+        heroImg="https://images.unsplash.com/photo-1698507961128-0959d8289d39?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D"
         title="Your Journey Your Story"
         text="Choose Your favorite destination"
         buttonText="Travel Plan"
