2173ef29705fa40b081b6220fc7209c56077d148
Adebozz <ademolaadeigbe3@gmail.com>
2023-11-01 17:43:00 +0100

    adding the footer to the pages

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -12,18 +12,18 @@ function Trip(){
             <div className="tripcard">
                 <TripData 
                 image={Trip1}
-                heading="Trip in Indonesia"
-                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. It consists of over 17,000 islands, inclusing Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea. "
+                heading="Trip in Iceland"
+                text="Iceland, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. It consists of over 17,000 islands, inclusing Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea. "
                 />
                 <TripData 
                 image={Trip2}
-                heading="Trip in Indonesia"
-                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. It consists of over 17,000 islands, inclusing Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea. "
+                heading="Trip in Malaysia"
+                text="Maleysia is a Southeast Asian country occupying parts of the Malay Peninsula and the island of Borneo. Its known for its beaches rainforests and mix of malay. Chinese, Indian and European cultural influences."
                 />
                 <TripData 
                 image={Trip3}
-                heading="Trip in Indonesia"
-                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans. It consists of over 17,000 islands, inclusing Sumatra, Java, Sulawesi, and parts of Borneo and New Guinea. "
+                heading="Trip in France"
+                text="France, in Western Europe, encompasses medieval cities, airplane villages and Maditerranean beaches. Paris, its capital is farmed for its fashion houses, classical art museums including the Louvre and monuments like the effel Towel. "
                 />
             </div>
         </div>
