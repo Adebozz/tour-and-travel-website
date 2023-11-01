1de3e0ddcd29c9637641cbccbf046f5b30d8fcbf
Adebozz <ademolaadeigbe3@gmail.com>
2023-11-01 10:40:44 +0100

    fouth commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero(props) {
     <div className={props.cName}>
       <img alt='HeroImg' src={props.heroImg} />
 
-      <div>
+      <div className='hero-text'>
         <h1>{props.title}</h1>
         <p>{props.text} </p>
         <a href={props.url} className={props.btnClass}>
