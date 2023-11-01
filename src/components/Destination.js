bd8d75860c74bc1c89928e584d98de4635312355
Adebozz <ademolaadeigbe3@gmail.com>
2023-11-01 15:41:03 +0100

    fifth commit

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,3 +1,7 @@
+import Mountain1 from "../assests/1 (7).jpg"
+import Mountain2 from "../assests/1 (8).jpg"
+import Mountain3 from "../assests/1 (9).jpg"
+import Mountain4 from "../assests/1 (10).jpg"
 import DestinationData from "./DestinationData";
 import "./DestinationStyles.css"
 
@@ -6,9 +10,27 @@ const Destination = () =>{
         <div className="destination">
             <h1>Popular Destination</h1>
             <p>Tours give you the opportunity to see alot within a time frame</p>
-           <DestinationData />
+           <DestinationData
+           className="first-des"
+           heading="Taal Volcano, Batangas"
+           text="  One of the mopst iconic views in Luzon, Mt Tall boasts a volcano inside a lake inaside an island. Of you fancy a closer look, the hike up to the creator is a mere 45 minutes, and is easy enpough for beginners. Guide's will assist you most of the way and you will see the peculiar environment found on an active colcano inclusing volcanic rocks and steam vents. The hikes can be dusty and hot so plan for an early morning trip, and then enwind with same bulalal before heading back home!
+           "
+           image1={Mountain1}
+           image2={Mountain2}
+           />
+
+<DestinationData
+            className="second-des"
+            image1={Mountain3}
+           image2={Mountain4}
+           heading="Mt. Daguldul; Batangas"
+           text="  One of the mopst iconic views in Luzon, Mt Tall boasts a volcano inside a lake inaside an island. Of you fancy a closer look, the hike up to the creator is a mere 45 minutes, and is easy enpough for beginners. Guide's will assist you most of the way and you will see the peculiar environment found on an active colcano inclusing volcanic rocks and steam vents. The hikes can be dusty and hot so plan for an early morning trip, and then enwind with same bulalal before heading back home!
+           "
+           
+           />
         </div>
-    )
-}
+        
+    );
+};
 
 export default Destination;
--- src/components/DestinationData.js
+++ src/components/DestinationData.js
@@ -1,23 +1,17 @@
-import Mountain1 from "../assests/1 (7).jpg"
-import Mountain2 from "../assests/1 (8).jpg"
-import Mountain3 from "../assests/1 (9).jpg"
-import Mountain4 from "../assests/1 (10).jpg"
 import { Component } from "react";
 import "./DestinationStyles.css"
 
 class DestinationData extends Component{
     render(){
         return(
-            <div className="first-des">
+            <div className={this.props.className}>
             <div className="des-text">
-                <h2>Taal Volcano, Batangas</h2>
-        <p>
-            One of the mopst iconic views in Luzon, Mt Tall boasts a volcano inside a lake inaside an island. Of you fancy a closer look, the hike up to the creator is a mere 45 minutes, and is easy enpough for beginners. Guide's will assist you most of the way and you will see the peculiar environment found on an active colcano inclusing volcanic rocks and steam vents. The hikes can be dusty and hot so plan for an early morning trip, and then enwind with same bulalal before heading back home!
-        </p>
+                <h2>{this.props.heading}</h2>
+        <p>{this.props.text}</p>
         </div>
         <div className="image">
-            <img alt="image1" src={Mountain1}/>
-            <img alt="image2" src={Mountain2}/>
+            <img alt="image1" src={this.props.image1}/>
+            <img alt="image2" src={this.props.image2}/>
         </div>
         </div>
         )
