fb14cea90c554906a44cb8d0275809491530e3d1
Adebozz <ademolaadeigbe3@gmail.com>
2023-11-06 23:36:30 +0100

    Updated info on about page

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AboutStyle.css"
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
--- src/routes/About.js
+++ src/routes/About.js
@@ -3,11 +3,13 @@ import Hero from "../components/Heroes";
 import AboutImage from "../assests/1 (10).jpg";
 import Footer from "../components/Footer";
 
+
 function About() {
   return (
     <>
       <Navbar />
       <Hero cName="hero-mid" heroImg={AboutImage} title="About" />
+      <About />
       <Footer />
     </>
   );
